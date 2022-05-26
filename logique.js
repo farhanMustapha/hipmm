8986fb011d41e731b3b205891a6951e474e09fa6
farhanMustapha <farhan.mustapha2016@gmail.com>
2022-05-26 12:52:32 +0100

    second commit

--- logique.js
+++
@@ -1,7 +0,0 @@
-function créationbouton(){
-    var btn =  document.createElement('button'); //créer le bouton
-    var input = document.getElementById('newevent');//récupérer l'input
-    btn.setAttribute('name', input.value); //lui donner son nom
-    input.parentNode.insertBefore(btn, input.nextSibling);//l'insérer dans le dom où on veut: ici après l'input 
-    //(insérer parmi les fils du parent de l'input, avant le suivant de l'input)
-}
